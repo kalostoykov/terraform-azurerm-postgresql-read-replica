2a8b4b5cd93f80e284116cb61e57f2fa8e785866
Kaloyan Stoykov <kalostoykov@users.noreply.github.com>
2020-02-05 14:54:43 +0200

    Remove primary server configuration changes
    Remove primary server configuration changes
    
    - Configuration changes
    
    On a newly created PostgreSQL server the replication is enabled by default
    and the ENABLE_REPLICATION step is failing because of that.
    Acccording the to [docs](https://docs.microsoft.com/en-gb/azure/postgresql/howto-read-replicas-cli#prepare-the-master-server).
    If you get "Invalid value given" that means that the replication is
    already enabled on that PostgreSQL server.
    
    - WARNING: External references from destroy provisioners are deprecated
    
    In order to fix that issue the self object must be referenced instead of
    passing the vars directly.

--- main.tf
+++ main.tf
@@ -5,42 +5,23 @@ resource "null_resource" "postgresql-read-replica" {
     postgresql_replica_server_name = var.postgresql_replica_server_name
   }
 
-  # enables replication on the primary server
-  provisioner "local-exec" {
-    command = <<ENABLE_REPLICATION
-az postgres server configuration set \
-  --resource-group ${var.resource_group_name} \
-  --server-name ${var.postgresql_primary_server_name} \
-  --name azure.replication_support --value REPLICA
-ENABLE_REPLICATION
-  }
-
-  # restart primary for change to take effect
-  provisioner "local-exec" {
-    command = <<RESTART_SERVER
-az postgres server restart \
-  --name ${var.postgresql_primary_server_name} \
-  --resource-group ${var.resource_group_name}
-RESTART_SERVER
-  }
-
   # create replica
   provisioner "local-exec" {
     command = <<CREATE_REPLICA
 az postgres server replica create \
-  --name ${var.postgresql_replica_server_name} \
-  --source-server ${var.postgresql_primary_server_name} \
-  --resource-group ${var.resource_group_name}
+  --name ${self.triggers.postgresql_replica_server_name} \
+  --source-server ${self.triggers.postgresql_primary_server_name} \
+  --resource-group ${self.triggers.resource_group_name}
 CREATE_REPLICA
   }
 
   provisioner "local-exec" {
-    when = "destroy" 
+    when = destroy
     command = <<DESTROY_REPLICA
 az postgres server delete \
-  --name ${var.postgresql_replica_server_name} \
-  --resource-group ${var.resource_group_name} \
+  --name ${self.triggers.postgresql_replica_server_name} \
+  --resource-group ${self.triggers.resource_group_name} \
   --yes
-DESTROY_REPLICA    
+DESTROY_REPLICA
   }
 }
